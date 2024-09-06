44509682a6f1cb324a234b0c039a407030770f1a
BSLAY <a.o.gladky@gmail.com>
2024-09-06 11:20:09 +0300

    test commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	fmt.Println("Сервер запущен")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/forfunphp/shortUrl
+
+go 1.21.3
