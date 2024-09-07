3a70effd51c57de61bd03f8e7f94002b8d3357e1
BSLAY <a.o.gladky@gmail.com>
2024-09-07 15:13:17 +0300

    iter12

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -46,7 +46,6 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusCreated)
 		w.Header().Set("Content-Type", "text/plain")
 		fmt.Fprintf(w, "http://localhost:8080/%s\n", shortURL)
-
 	}
 
 	if r.Method == http.MethodGet {
