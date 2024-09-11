4249e775c189b3c745a015c1fbff8db56b4f1bf1
BSLAY <a.o.gladky@gmail.com>
2024-09-11 20:39:36 +0300

    fix3

--- config/config.go
+++ config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"fmt"
 	"github.com/spf13/pflag"
+	"strconv"
 )
 
 type Config struct {
@@ -17,6 +19,11 @@ func (c *Config) Init() error {
 	pflag.StringVarP(&c.HTTPAddr, "http-addr", "a", "localhost:8080", "адрес прослушивания HTTP-сервера")
 	pflag.StringVarP(&c.BaseURL, "base-url", "b", "http://localhost:8080/", "базовый адрес для сокращенных URL")
 	pflag.Parse()
+
+	port, err := strconv.Atoi(c.HTTPAddr[len(c.HTTPAddr)-4:])
+	if err != nil || port < 0 || port > 8888 {
+		return fmt.Errorf("недопустимый порт HTTP-сервера: %s", c.HTTPAddr)
+	}
 
 	return nil
 }
