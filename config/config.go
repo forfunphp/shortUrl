70918eccd317a3a769bab59fe4fae8f95c157457
BSLAY <a.o.gladky@gmail.com>
2024-09-11 20:47:34 +0300

    fix4

--- config/config.go
+++ config/config.go
@@ -17,12 +17,18 @@ func NewConfig() *Config {
 
 func (c *Config) Init() error {
 	pflag.StringVarP(&c.HTTPAddr, "http-addr", "a", "localhost:8080", "адрес прослушивания HTTP-сервера")
-	pflag.StringVarP(&c.BaseURL, "base-url", "b", "http://localhost:8080/", "базовый адрес для сокращенных URL")
+	pflag.StringVarP(&c.BaseURL, "base-url", "b", "http://localhost:8000/", "базовый адрес для сокращенных URL")
 	pflag.Parse()
 
 	port, err := strconv.Atoi(c.HTTPAddr[len(c.HTTPAddr)-4:])
 	if err != nil || port < 0 || port > 8888 {
 		return fmt.Errorf("недопустимый порт HTTP-сервера: %s", c.HTTPAddr)
+	}
+
+	port, err = strconv.Atoi(c.BaseURL[len(c.BaseURL)-5 : len(c.BaseURL)-1])
+
+	if err != nil || port < 0 || port > 8000 {
+		return fmt.Errorf("недопустимый порт базового сервера: %s", c.HTTPAddr)
 	}
 
 	return nil
