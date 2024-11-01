aa0419eb12a4da5d1b6940f1aa70929b01bf4f46
BSLAY <a.o.gladky@gmail.com>
2024-11-01 08:48:41 +0300

    fix3

--- internal/handlers/shorten.go
+++ internal/handlers/shorten.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"io"
@@ -29,12 +30,24 @@ func Shorten(c *gin.Context) {
 
 	shortURL := reduceURL()
 
-	fmt.Printf("lin2klink2link---")
+	fmt.Printf("Здесь json--->")
 	fmt.Printf("Парсированный URL: %s\n", parsedURL.String())
 
 	URLMap[shortURL] = URLPair{parsedURL, shortURL}
 
-	result := ShortURL{ShortURL: Cfg.BaseURL + "/" + shortURL}
-	fmt.Printf("Здесь json-----")
-	c.JSON(http.StatusCreated, result)
+	var resp ShortURL                            // Инициализируем структуру ShortURL
+	resp.ShortURL = Cfg.BaseURL + "/" + shortURL // Заполняем поле ShortURL
+
+	// Кодируем ответ в JSON
+	jsonData, err := json.Marshal(resp)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Отправляем ответ
+	c.JSON(http.StatusCreated, string(jsonData))
+
+	fmt.Printf("<----Здесь json")
+
 }
