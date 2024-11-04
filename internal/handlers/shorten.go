77f7d57807b42958b711b0de9f9d9a4157372483
BSLAY <a.o.gladky@gmail.com>
2024-11-04 16:16:20 +0300

    fix37

--- internal/handlers/shorten.go
+++ internal/handlers/shorten.go
@@ -67,7 +67,7 @@ func Shorten(c *gin.Context) {
 			zap.String("contentType2", contentType), // Добавляем parsedURL
 			zap.Int("statusCode2", c.Writer.Status()),
 		)
-		c.JSON(http.StatusInternalServerError, jsonData)
+		c.JSON(http.StatusCreated, jsonData)
 	} else if contentType == "application/x-gzip" {
 		c.Data(http.StatusCreated, "application/x-gzip", jsonData)
 	}
