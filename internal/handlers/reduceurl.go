10dc6ce0ec6e7a8c1f727bdf28df86c4a92132d9
BSLAY <a.o.gladky@gmail.com>
2024-11-05 19:12:57 +0300

    ss

--- internal/handlers/reduceurl.go
+++ internal/handlers/reduceurl.go
@@ -46,6 +46,13 @@ func ReduceURL(c *gin.Context) {
 	URL := string(body)
 	parsedURL, err := url.Parse(URL)
 
+	logger2, _ := zap.NewDevelopment()
+	defer logger2.Sync()
+	logger2.Info("Request xxxxx",
+		zap.String("fullURL", URL),           // Добавляем полный URL
+		zap.Int("Status", c.Writer.Status()), // Добавляем parsedURL
+	)
+
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{URL + "error": "Не спарсил URL"})
 		return
