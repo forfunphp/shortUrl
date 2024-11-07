ad1d5864c597a94a38964f1e058f995c8e37a563
BSLAY <a.o.gladky@gmail.com>
2024-11-07 13:01:39 +0300

    апроf

--- internal/handlers/redirect.go
+++ internal/handlers/redirect.go
@@ -2,12 +2,21 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 	"net/http"
 )
 
 func Redirect(c *gin.Context) {
 	shortURL := c.Param("shortURL")
 	URLPair, ok := URLMap[shortURL]
+
+	filePath := Cfg.EnvFilePath
+	logger2, _ := zap.NewDevelopment()
+	defer logger2.Sync()
+	logger2.Info("Request proce00003",
+		zap.String("fullURL", shortURL),  // Добавляем полный URL
+		zap.String("filePath", filePath), // Добавляем полный URL
+	)
 
 	if !ok {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Нет урла"})
