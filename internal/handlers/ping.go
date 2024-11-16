0569167ecfc7104e1c5bf6d42b10a1c24f533552
BSLAY <a.o.gladky@gmail.com>
2024-11-16 13:57:02 +0300

    fix2v345

--- internal/handlers/ping.go
+++ internal/handlers/ping.go
@@ -1,28 +1,28 @@
-package handlers
+package main
 
 import (
 	"database/sql"
 	"flag"
-	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
-	_ "github.com/lib/pq"
-	"go.uber.org/zap"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql" // Импорт драйвера MySQL
+	"go.uber.org/zap"
 )
 
 var db *sql.DB
-var err error
+var logger *zap.Logger
 
-func Ping(c *gin.Context) {
-
-	logger, _ := zap.NewDevelopment()
+func init() {
+	var err error
+	logger, err = zap.NewProduction()
+	if err != nil {
+		log.Fatalf("can't initialize logger: %v", err)
+	}
 	defer logger.Sync()
-	logger.Info("Request processed1ss",
-		zap.String("fullURL", "2222"), // Добавляем полный URL
-
-	)
 
 	dsn := os.Getenv("DATABASE_DSN")
 	if dsn == "" {
@@ -36,5 +36,23 @@ func Ping(c *gin.Context) {
 	}
 
 	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		logger.Fatal("Ошибка при подключении к базе данных", zap.Error(err))
+	}
+
+	err = db.Ping()
+	if err != nil {
+		logger.Fatal("Ошибка при проверке подключения к базе данных", zap.Error(err))
+	}
+	logger.Info("Подключение к базе данных установлено.")
+}
+
+func Ping(c *gin.Context) {
+	err := db.Ping()
+	if err != nil {
+		logger.Error("Ошибка при проверке подключения к базе данных", zap.Error(err))
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
 	c.String(http.StatusOK, "OK")
 }
