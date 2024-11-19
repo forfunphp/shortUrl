c12ad728fcbf7f336f02a3e5adb962bfa9894661
BSLAY <a.o.gladky@gmail.com>
2024-11-19 11:07:47 +0300

    xxxx

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -41,18 +41,11 @@ type URLData struct {
 }
 
 func parseFlags() {
-	flag.StringVar(&flagRunAddr, "a", ":8080", "address and port to run server")
-	flag.StringVar(&flagLogLevel, "l", "info", "log level")
+
 	// обрабатываем аргумент -d
 	flag.StringVar(&flagDatabaseURI, "d", "", "database URI")
 	flag.Parse()
 
-	if envRunAddr := os.Getenv("RUN_ADDR"); envRunAddr != "" {
-		flagRunAddr = envRunAddr
-	}
-	if envLogLevel := os.Getenv("LOG_LEVEL"); envLogLevel != "" {
-		flagLogLevel = envLogLevel
-	}
 	// обрабатываем переменную окружения DATABASE_URI
 	if envDatabaseURI := os.Getenv("DATABASE_URI"); envDatabaseURI != "" {
 		flagDatabaseURI = envDatabaseURI
