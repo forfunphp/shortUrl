fa445e0d38dafaf12ed7047e971383bfdb435524
BSLAY <a.o.gladky@gmail.com>
2025-01-17 09:07:53 +0700

    vvvv

--- config/config.go
+++ config/config.go
@@ -9,10 +9,11 @@ import (
 )
 
 type Config struct {
-	HTTPAddr    string
-	BaseURL     string
-	EnvFilePath string
-	Databes     string
+	HTTPAddr       string
+	BaseURL        string
+	EnvFilePath    string
+	Databes        string
+	UseFileStorage bool
 }
 
 func NewConfig() *Config {
@@ -20,30 +21,49 @@ func NewConfig() *Config {
 }
 
 func (c *Config) Init() error {
+	c.Databes = ""
+	c.EnvFilePath = "urls.json"
+	c.UseFileStorage = false
 
 	c.HTTPAddr = os.Getenv("SERVER_ADDRESS")
 	c.BaseURL = os.Getenv("BASE_URL")
 	c.EnvFilePath = os.Getenv("FILE_STORAGE_PATH")
-	c.Databes = os.Getenv("DATABASE_DSN")
+
+	if os.Getenv("DATABASE_DSN") != "" {
+		c.Databes = os.Getenv("DATABASE_DSN")
+		c.UseFileStorage = false // Отключаем использование файла, если есть БД
+	}
 
 	pflag.StringVarP(&c.HTTPAddr, "http-addr", "a", "localhost:8080", "адрес прослушивания HTTP-сервера")
 	pflag.StringVarP(&c.BaseURL, "base-url", "b", "http://localhost:8080", "базовый адрес для сокращенных URL")
 	pflag.StringVarP(&c.EnvFilePath, "f", "f", "urls.json", "Путь к файлу для хранения URL")
-	pflag.StringVarP(&c.Databes, "d", "d", "", "Строка с адресом подключения к БД")
+	pflag.StringVar(&c.Databes, "d", c.Databes, "Строка с адресом подключения к БД")
 	pflag.Parse()
 
-	fmt.Println("34t4tg4g444g4g")
-	fmt.Println(c.Databes)
-
-	if os.Getenv("FILE_STORAGE_PATH") != "" {
-		c.EnvFilePath = os.Getenv("FILE_STORAGE_PATH")
+	// Логика для определения источника хранения
+	if c.Databes == "" {
+		if c.EnvFilePath != "" && c.EnvFilePath != "urls.json" {
+			c.UseFileStorage = true
+		} else {
+			c.UseFileStorage = false // По умолчанию используем память
+		}
+	} else {
+		c.UseFileStorage = false // Если есть БД, то не используем файл
 	}
 
-	fmt.Println("conn2")
+	fmt.Printf("HTTPAddr: %s\n", c.HTTPAddr)
+	fmt.Printf("BaseURL: %s\n", c.BaseURL)
+	fmt.Printf("EnvFilePath: %s\n", c.EnvFilePath)
+	fmt.Printf("Databes: %s\n", c.Databes)
+	fmt.Printf("UseFileStorage: %t\n", c.UseFileStorage)
 
-	if os.Getenv("FILE_STORAGE_PATH") != "" {
-		c.EnvFilePath = os.Getenv("FILE_STORAGE_PATH")
-	}
+	//if os.Getenv("FILE_STORAGE_PATH") != "" {
+	//	c.EnvFilePath = os.Getenv("FILE_STORAGE_PATH")
+	//}
+
+	//if os.Getenv("DATABASE_DSN") != "" {
+	//	c.Databes = os.Getenv("DATABASE_DSN")
+	//}
 
 	port, err := strconv.Atoi(c.HTTPAddr[len(c.HTTPAddr)-4:])
 	if err != nil || port < 0 || port > 65535 {
