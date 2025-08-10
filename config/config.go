05e329d62aabbd2e74f3fdb72de3113b1f6571a0
BSLAY <a.o.gladky@gmail.com>
2025-08-10 16:40:03 +0300

    777777777

--- config/config.go
+++ config/config.go
@@ -42,7 +42,7 @@ func (c *Config) Init() error {
 
 	if c.Databes != "" {
 		//	c.Databes = os.Getenv("DATABASE_DSN")
-		log.Println("3333333333333133332")
+		log.Println("333333333333313d3332")
 
 		DB, err := sql.Open("postgres", c.Databes) // Замените "postgres" именем вашего драйвера
 		if err != nil {
--- internal/handlers/reduceurl.go
+++ internal/handlers/reduceurl.go
@@ -162,7 +162,7 @@ func ReduceURL(c *gin.Context) {
 	if err != nil {
 		if pgErr, ok := err.(*pq.Error); ok {
 			if pgErr.Code == pgerrcode.UniqueViolation {
-				log.Println("9999999999999999999999")
+				log.Println("99999999999999d99999999")
 
 				// Handle unique violation (409 Conflict)
 				existingShortURL, err := getExistingShortURL(c.Request.Context(), parsedURL.String())
